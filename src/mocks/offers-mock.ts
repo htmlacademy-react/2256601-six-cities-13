c7947b45c6208ee28ca2a87dcea390680b013294
Николай Бабаркин <nikola_@bk.ru>
2023-07-23 21:27:26 +0300

    feat: add offer-object-mock

--- src/mocks/offers-mock.ts
+++ src/mocks/offers-mock.ts
@@ -2,56 +2,35 @@ import { OffersList } from '../types/offers-list';
 
 export const offersMock: OffersList[] = [
   {
-    id: '1ed853d2-d662-4bb7-be8c-21ec3b3278b1',
-    title: 'Amazing and Extremely Central Flat',
+    id: 'a724fe10-978c-4b24-b5aa-c033830de2cc',
+    title: 'Penthouse, 4-5 rooms + 5 balconies',
     type: 'apartment',
-    price: 118,
+    price: 471,
     city: {
-      name: 'Brussels',
+      name: 'Cologne',
       location: {
-        latitude: 50.846557,
-        longitude: 4.351697,
+        latitude: 50.938361,
+        longitude: 6.959974,
         zoom: 13
-      }},
+      }
+    },
     location: {
-      latitude: 50.827557,
-      longitude: 4.336697,
+      latitude: 50.941361,
+      longitude: 6.956974,
       zoom: 16
     },
     isFavorite: false,
     isPremium: true,
-    rating: 1.9,
+    rating: 2,
     previewImage: 'https://13.design.pages.academy/static/hotel/11.jpg'
   },
   {
-    id: '34f50f68-803c-43a9-8d59-9556fb9c0eaa',
-    title: 'The house among olive',
+    id: '937e13a8-d128-42fc-9b67-b8b29e41b0dc',
+    title: 'The Joshua Tree House',
     type: 'hotel',
-    price: 197,
+    price: 178,
     city: {
       name: 'Paris',
-      location: {
-        latitude: 52.37454,
-        longitude: 4.897976,
-        zoom: 13
-      }},
-    location: {
-      latitude: 52.367540000000005,
-      longitude: 4.883976,
-      zoom: 16
-    },
-    isFavorite: true,
-    isPremium: false,
-    rating: 1.1,
-    previewImage: 'https://13.design.pages.academy/static/hotel/10.jpg'
-  },
-  {
-    id: 'c99c9239-7836-4115-a767-ee81c4b835ad',
-    title: 'Amazing and Extremely Central Flat',
-    type: 'room',
-    price: 224,
-    city: {
-      name: 'Amsterdam',
       location: {
         latitude: 48.85661,
         longitude: 2.351499,
@@ -62,16 +41,16 @@ export const offersMock: OffersList[] = [
       longitude: 2.342499,
       zoom: 16
     },
-    isFavorite: true,
-    isPremium: false,
-    rating: 2,
-    previewImage: 'https://13.design.pages.academy/static/hotel/3.jpg'
+    isFavorite: false,
+    isPremium: true,
+    rating: 1.7,
+    previewImage: 'https://13.design.pages.academy/static/hotel/10.jpg'
   },
   {
-    id: 'e422297a-1bd1-4ef9-aeac-3af3c02eaf7f',
-    title: '"Canal View Prinsengracht',
-    type: 'hotel',
-    price: 140,
+    id: 'e20ec930-ad5f-4bf1-a7fe-83de62cc0ac0',
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'room',
+    price: 210,
     city: {
       name: 'Cologne',
       location: {
@@ -80,13 +59,35 @@ export const offersMock: OffersList[] = [
         zoom: 13
       }},
     location: {
-      latitude: 50.932361,
-      longitude: 6.937974,
+      latitude: 50.947361,
+      longitude: 6.9799739999999995,
       zoom: 16
     },
-    isFavorite: true,
+    isFavorite: false,
     isPremium: true,
-    rating: 4.2,
+    rating: 1.3,
+    previewImage: 'https://13.design.pages.academy/static/hotel/18.jpg'
+  },
+  {
+    id: 'cb73f9eb-7a6f-49e7-9758-94ec697d85bc',
+    title: 'House in countryside',
+    type: 'house',
+    price: 945,
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 50.938361,
+        longitude: 6.959974,
+        zoom: 13
+      }},
+    location: {
+      latitude: 50.960361,
+      longitude: 6.967974,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 3.3,
     previewImage: 'https://13.design.pages.academy/static/hotel/20.jpg'
   },
 ];
