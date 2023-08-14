6e6dce0cd45fba32f0ef7fb39de9043052f39f2d
Николай Бабаркин <nikola_@bk.ru>
2023-08-14 20:11:00 +0300

    feat: add sort

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -1,18 +1,56 @@
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { fetchOffers, setSortType } from '../../store/actions';
+import { useState, MouseEvent } from 'react';
+import { sorting } from '../../utils';
+import { SortingMap } from '../../const';
+import classNames from 'classnames';
+import { Sorting } from '../../types/sorting';
+
 export function Sort () {
+  const [isOpened, setIsOpened] = useState(false);
+  const activeSortType = useAppSelector((state) => state.sortType);
+  const offersList = useAppSelector((state) => state.offers);
+  const dispatch = useAppDispatch();
+
+  const clickHandler = (item: Sorting) => {
+    dispatch(setSortType(item));
+    dispatch(fetchOffers(sorting[item](offersList)));
+  };
+
+  const spanClickHandler = (evt: MouseEvent<HTMLFormElement>) => {
+    evt.stopPropagation();
+    setIsOpened((prevIsOpened) => !prevIsOpened);
+  };
+
+  const keydownHandler = (evt: KeyboardEvent) => {
+    if (evt.key === 'Escape' && isOpened) {
+      evt.preventDefault();
+      setIsOpened(false);
+    }
+  };
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
-        Popular
+      <span className="places__sorting-type" tabIndex={0} onClick={spanClickHandler}>
+        {SortingMap[activeSortType]}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-        <li className="places__option" tabIndex={0}>Price: low to high</li>
-        <li className="places__option" tabIndex={0}>Price: high to low</li>
-        <li className="places__option" tabIndex={0}>Top rated first</li>
+      <ul className={classNames('places__options', 'places__options--custom', {'places__options--opened': isOpened})}>
+        {
+          (Object.entries(SortingMap) as [Sorting, (typeof SortingMap)[Sorting]][]).map(([type, label]) => (
+            <li className={classNames('places__option', {'places__option--active' : type === activeSortType})}
+              key={type}
+              tabIndex={0}
+              onClick={() => clickHandler(type)}
+              onKeyDown={() => keydownHandler}
+            >
+              {label}
+            </li>
+          ))
+        }
       </ul>
     </form>
   );
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,22 +1,28 @@
 import {createReducer} from '@reduxjs/toolkit';
 import { OfferListItem } from '../types/offer-list-item';
-import { fetchOffers, setActiveCity } from './actions';
+import { fetchOffers, setActiveCity, setSortType } from './actions';
 import { offersMock } from '../mocks/offers-mock';
+import { Sorting } from '../types/sorting';
 
 const initialState: {
   offers: OfferListItem[];
   city: string | undefined ;
+  sortType: Sorting;
 } = {
   offers: offersMock,
   city: 'Paris',
+  sortType: 'Popular'
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(fetchOffers, (state) => {
-      state.offers = offersMock;
+    .addCase(fetchOffers, (state, action) => {
+      state.offers = action.payload;
     })
     .addCase(setActiveCity, (state, action) => {
       state.city = action.payload;
+    })
+    .addCase(setSortType, (state, action) => {
+      state.sortType = action.payload;
     });
 });
